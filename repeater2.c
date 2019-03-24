5e12d34d3a73c4594a297a645f46b26a5a79e8bd
ydzu <yagodzu@gmail.com>
2019-03-24 17:39:41 -0300

    explicación del problema en repeat2.c

--- repeater2.c
+++ repeater2.c
@@ -17,21 +17,21 @@ int* createCanvas(){
   int canvas[10][10]={0};
   int *ptrCanvas, *copyCanvas;
 
-  ptrCanvas = (int*)malloc (100 * sizeof(int));
-  ptrCanvas = &canvas[0][0];
+  ptrCanvas = (int*)malloc (100 * sizeof(int)); //como no podía acceder al array 2D desde printCanvas, hice esto por si era un problema de memoria
+  ptrCanvas = &canvas[0][0]; //apunto a la primer posición del array 2D
   copyCanvas = ptrCanvas;
 
-  for (i=0; i<10; i++){
+  for (i=0; i<10; i++){        //para asegurarme de que estoy manipulando bien el array 2D hice esto. FUNCIONA BIEN
     for (j=0; j<10; j++){
       canvas[i][j]=3;
     }
   }
 
-  canvas[0][0]=1;
+  canvas[0][0]=1;  //también para asegurarme de que las posiciones funcionan OK. FUNCIONA BIEN
   canvas[1][1]=1;
   canvas[4][6]=1;
 
-  for (i=0; i<10; i++){
+  for (i=0; i<10; i++){               //como no puedo hacer esto mismo desde printCanvas() lo hice acá con el ptr y FUNCIONA BIEN
     for (j=0; j<10; j++){
       printf("%d",*ptrCanvas);
       ptrCanvas++;
@@ -44,10 +44,10 @@ int* createCanvas(){
 
 void printCanvas(int *a){
   int i,j;
-
-  for (i=0; i<10; i++){
+                                  // NO PUEDO ACCEDER AL ARRAY 2D DE NINGUNA MANERAAA. NO SE QUÉ ESTOY HACIENDO MAL
+  for (i=0; i<10; i++){          //  lrpm acá es donde nada funciona. solamente me da bien la primer posición del puntero. a partir de la segunda da valore random
     for (j=0; j<10; j++){
-      printf("%d",*(a + (i * 10 + j)));
+      printf("%d",*(a + (i * 10 + j)));  //itenté acceder de esta forma con ROW y COLUMN. también intenté moviendo el ptr *a de forma lineal.
     }
     printf("\n");
   }
